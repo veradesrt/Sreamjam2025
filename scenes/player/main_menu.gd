a347e7022c57efd1a4a55994d1d3e853490664d8
Cranile <nicolaslcrapanzano@gmail.com>
2025-10-10 17:10:35 -0300

    added some ui and basic scripting to handle it

---
+++ scenes/player/main_menu.gd
@@ -0,0 +1,44 @@
+extends Control
+
+@onready var menu_container: Panel = $menu_container
+@onready var dialogue_container: Control = $dialogue_container
+
+@onready var player: Player = $".."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	close_all()
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
+
+func open_menu():
+	player.change_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	menu_container.visible = true
+
+func close_menu():
+	player.change_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	menu_container.visible = false
+
+func open_dialogue():
+	player.change_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	dialogue_container.visible = true
+
+func close_dialogue():
+	player.change_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	dialogue_container.visible = false
+
+func close_all():
+	menu_container.visible = false
+	dialogue_container.visible = false
+	player.change_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _on_resume_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,31 +1,45 @@
 extends CharacterBody3D
+class_name Player
 
+## container for the camera
 @onready var head: Node3D = $head
 ## used for checking the ground material
 @onready var ground_check: RayCast3D = $ground_check
+## sound player for the player footsteps
 @onready var footsteps_player: AudioStreamPlayer3D = $footsteps_player
 
+@onready var main_menu: Control = $UI
+
+## used to control the player movement speed
 const SPEED = 5.0
+## Unused
 const JUMP_VELOCITY = 4.5
 
+## mouse sensitivity, used for camera control
 @export var mouse_sens : float = 0.001
 
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	## on startup set the mouse mode to captured so the player can look around
+	change_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+## function used to change the mouse mode from a single place
+func change_mouse_mode(new_mode):
+	Input.mouse_mode = new_mode
+
+func open_exit_menu():
+	
+	pass
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
-		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		else:
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		change_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		pass
 	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(event.relative.x * -1 *mouse_sens)
 		head.rotate_x(event.relative.y * -1 *mouse_sens)
 		head.rotation.x = clamp(head.rotation.x ,deg_to_rad(-90),deg_to_rad(90))
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -36,8 +50,12 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		##only if the player is moving
+		##check if the ground check can detect ground
 		var collider = ground_check.get_collider()
+		##if raycast cant detect anything (example: player is on air) skip
 		if collider != null:
+			## get the groups of the detected collider
 			var ground_groups : Array = collider.get_groups()
 			if ground_groups.has("concrete"):
 				#print("walking on concrete")
