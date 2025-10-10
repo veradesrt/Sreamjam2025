0b5349acc5ff1cce15c55beacd343fd711c689cc
Gian L. Mattera <gianlive.com.ar@live.com>
2025-10-10 13:18:03 -0300

    added DialogueGlobalEmitter and more parameters to Text

---
+++ scripts/dialogue/dialogue/DialogueGlobalEmitter.gd
@@ -0,0 +1,7 @@
+extends Node
+
+signal start_dialog(dialogue : Dialogue, )
+signal force_end_dialog
+
+signal has_started
+signal has_ended
--- scripts/dialogue/text/Text.gd
+++ scripts/dialogue/text/Text.gd
@@ -2,4 +2,8 @@ class_name Text
 extends Resource
 
 @export_multiline var content : String
+
+@export_group("Options")
 @export var speed : float = 1.0
+@export var can_skip : bool = true
+@export var can_skip_outline : bool = false
