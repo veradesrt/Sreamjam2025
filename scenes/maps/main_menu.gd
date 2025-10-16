bdf313c3a34051f38d136ca7a4580ba690f80b0b
Cranile <nicolaslcrapanzano@gmail.com>
2025-10-15 23:55:55 -0300

    made a lot of changes. added ambience sounds, added ui sounds, now ui does noise, addedmodels for cutscene, added disco building

--- scenes/maps/main_menu.gd
+++ scenes/maps/main_menu.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 @onready var options: Control = $UI/options
 
+@onready var hover_sound: AudioStreamPlayer = $hover_sound
+@onready var click_sound: AudioStreamPlayer = $click_sound
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,18 +15,34 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_play_pressed() -> void:
+	
+	SceneChanger.play_sound_across_scenes()
 	SceneChanger.request_scene_change("cutscene")
 
 func _on_settings_pressed() -> void:
+	click_sound.pitch_scale = 1
+	click_sound.play()
 	enable_options_menu()
 
 func _on_exit_pressed() -> void:
+	click_sound.pitch_scale = 1
+	click_sound.play()
 	get_tree().quit()
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	click_sound.pitch_scale = 1
+	click_sound.play()
 
 func enable_options_menu():
 	options.visible = true
 func disable_options_menu():
 	options.visible = false
+
+func on_hover_sound()->void:
+	hover_sound.play()
+
+
+func _on_close_options_pressed() -> void:
+	click_sound.pitch_scale = 0.5
+	click_sound.play()
+	disable_options_menu()
--- scripts/scene_changer.gd
+++ scripts/scene_changer.gd
@@ -4,12 +4,18 @@ const MAIN_MENU = preload("uid://cm5fdk81bqipp")
 const CUTSCENE = preload("uid://dm8k7cg8r2lwo")
 const MAIN_LEVEL = preload("uid://c78bdq77ygnhk")
 
+const PLAY_GAME_SOUND = preload("uid://b6hyllgphumlx")
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
 	pass
+
+func play_sound_across_scenes():
+	var sound :AudioStreamPlayer = PLAY_GAME_SOUND.instantiate()
+	add_child(sound)
+	sound.play()
 
 func request_scene_change(scene_name:String):
 	if scene_name == "main_menu":
